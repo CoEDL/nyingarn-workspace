af9b102a1a161aff81bd792670fa581237051f6d
Marco La Rosa <m@lr.id.au>
2021-08-13 10:15:59 +1000

    restore user data in vuex if loggedin

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,3 +3,27 @@
         <router-view />
     </div>
 </template>
+
+<script>
+import { tokenSessionKey, getLocalStorage } from "@/components/storage";
+
+export default {
+    data() {
+        return {};
+    },
+    mounted() {
+        this.setup();
+    },
+    methods: {
+        async setup() {
+            let isAuthed = await this.$http.get({ route: "/authenticated" });
+            if (isAuthed.status === 200) {
+                let { token } = getLocalStorage({ key: tokenSessionKey });
+
+                let userData = JSON.parse(atob(token.split(".")[1]));
+                this.$store.commit("setUserData", userData);
+            }
+        },
+    },
+};
+</script>

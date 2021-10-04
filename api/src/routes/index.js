d4189e2c7c0032ce86bc953da758cec1f1d1bc86
Marco La Rosa <m@lr.id.au>
2021-10-05 08:32:11 +1100

    require recaptcha before enabling auth methods
    commit 0b7baf5403c98e58ccf640cca654ef303bcf8064
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Tue Oct 5 08:31:47 2021 +1100
    
        implement recaptcha on login page before showing auth methods
    
    commit ab6ee3d575672ebeb6f696f7af369b7d60c4fa73
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Tue Oct 5 08:31:25 2021 +1100
    
        implement handler to check recaptcha response

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 // these endpoints will only return data they are responsible for
 //
 import models from "../models";
-import { UnauthorizedError } from "restify-errors";
+import { UnauthorizedError, ForbiddenError } from "restify-errors";
 import { setupRoutes as setupUserRoutes } from "./user";
 import { setupRoutes as setupAuthRoutes } from "./auth";
 import { setupRoutes as setupItemRoutes } from "./item";
@@ -60,6 +60,29 @@ export function setupRoutes({ server }) {
             next();
         })
     );
+    server.post("/recaptcha", async (req, res, next) => {
+        let configuration = await loadConfiguration();
+        const body = {
+            secret: configuration.api.services.recaptcha.secretKey,
+            response: req.body.token,
+        };
+        let response = await global.fetch(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${body.secret}&response=${body.response}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            }
+        );
+        let success = (await response.json()).success;
+        if (!success) {
+            return next(new ForbiddenError());
+        }
+        res.send({});
+        next();
+    });
     setupUserRoutes({ server });
     setupAuthRoutes({ server });
     setupItemRoutes({ server });

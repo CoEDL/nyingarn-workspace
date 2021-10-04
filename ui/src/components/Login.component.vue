08ca2d90c1d330c1320501aa05579ea05a07bb57
Marco La Rosa <m@lr.id.au>
2021-10-05 08:32:11 +1100

    require recaptcha before enabling auth methods
    commit 0b7baf5403c98e58ccf640cca654ef303bcf8064
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Tue Oct 5 08:31:47 2021 +1100
    
        implement recaptcha on login page before showing auth methods
    
    commit ab6ee3d575672ebeb6f696f7af369b7d60c4fa73
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Tue Oct 5 08:31:25 2021 +1100
    
        implement handler to check recaptcha response

--- ui/src/components/Login.component.vue
+++ ui/src/components/Login.component.vue
@@ -3,27 +3,54 @@
         <div class="w-full m-auto">
             <div class="flex flex-row">
                 <div
-                    class="w-1/2 text-right pr-4 text-gray-600 text-4xl flex flex-col justify-center"
+                    class="
+                        w-1/2
+                        text-right
+                        pr-4
+                        text-gray-600 text-4xl
+                        flex flex-col
+                        justify-center
+                    "
                 >
                     <div>
                         {{ siteName }}
                     </div>
                 </div>
                 <div class="w-1/2 pl-4 flex flex-col space-y-4">
-                    <oauth-login-component
-                        v-if="enableAafLogin"
-                        provider="aaf"
-                        image="aaf.png"
-                        button-text="Login with the AAF"
-                        class="border-l-4 border-solid p-4 cursor-pointer hover:border-yellow-400 hover:bg-yellow-100 focus:ring-2 focus:ring-yellow-100"
-                    />
-                    <oauth-login-component
-                        v-if="enableGoogleLogin"
-                        provider="google"
-                        image="google.png"
-                        button-text="Login with Google"
-                        class="border-l-4 border-solid p-4 cursor-pointer hover:border-yellow-400 hover:bg-yellow-100 focus:ring-2 focus:ring-yellow-100"
-                    />
+                    <div
+                        ref="recaptcha"
+                        :data-sitekey="recaptchaSiteKey"
+                        :data-callback="handleRecaptcha"
+                        v-if="!enableAuthenticationMethods"
+                    ></div>
+                    <span v-if="enableAuthenticationMethods">
+                        <oauth-login-component
+                            v-if="enableAafLogin"
+                            provider="aaf"
+                            image="aaf.png"
+                            button-text="Login with the AAF"
+                            class="
+                                border-l-4 border-solid
+                                p-4
+                                cursor-pointer
+                                hover:border-yellow-400 hover:bg-yellow-100
+                                focus:ring-2 focus:ring-yellow-100
+                            "
+                        />
+                        <oauth-login-component
+                            v-if="enableGoogleLogin"
+                            provider="google"
+                            image="google.png"
+                            button-text="Login with Google"
+                            class="
+                                border-l-4 border-solid
+                                p-4
+                                cursor-pointer
+                                hover:border-yellow-400 hover:bg-yellow-100
+                                focus:ring-2 focus:ring-yellow-100
+                            "
+                        />
+                    </span>
                 </div>
             </div>
         </div>
@@ -32,6 +59,9 @@
 
 <script>
 import { defineAsyncComponent } from "vue";
+import HTTPService from "@/http.service";
+const httpService = new HTTPService();
+
 export default {
     components: {
         OauthLoginComponent: defineAsyncComponent(() =>
@@ -41,6 +71,8 @@ export default {
     data() {
         return {
             siteName: this.$store.state.configuration.ui.siteName,
+            recaptchaSiteKey: this.$store.state.configuration.ui.recaptcha.siteKey,
+            enableAuthenticationMethods: false,
         };
     },
     computed: {
@@ -51,7 +83,22 @@ export default {
             return this.$store.state.configuration.authentication.includes("aaf");
         },
     },
-    mounted() {},
-    methods: {},
+    mounted() {
+        this.initRecaptcha();
+    },
+    methods: {
+        initRecaptcha() {
+            grecaptcha.render(this.$refs.recaptcha, {
+                sitekey: this.recaptchaSiteKey,
+                callback: this.handleRecaptcha,
+            });
+        },
+        async handleRecaptcha(token) {
+            let response = await httpService.post({ route: "/recaptcha", body: { token } });
+            if (response.status === 200) {
+                this.enableAuthenticationMethods = true;
+            }
+        },
+    },
 };
 </script>

542e2c5a6e3bf508a5c75b545d7ef2d3b22b2dd7
Marco La Rosa <m@lr.id.au>
2021-10-08 09:16:12 +1100

    refactor and DRY up

--- ui/src/components/Login.component.vue
+++ ui/src/components/Login.component.vue
@@ -17,40 +17,20 @@
                     </div>
                 </div>
                 <div class="w-1/2 pl-4 flex flex-col space-y-4">
-                    <!-- <div
-                        ref="recaptcha"
-                        :data-sitekey="recaptchaSiteKey"
-                        :data-callback="handleRecaptcha"
-                        v-if="!enableAuthenticationMethods"
-                    ></div> -->
-                    <span v-if="enableAuthenticationMethods">
-                        <oauth-login-component
-                            v-if="enableAafLogin"
-                            provider="aaf"
-                            image="aaf.png"
-                            button-text="Login with the AAF"
-                            class="
-                                border-l-4 border-solid
-                                p-4
-                                cursor-pointer
-                                hover:border-yellow-400 hover:bg-yellow-100
-                                focus:ring-2 focus:ring-yellow-100
-                            "
-                        />
-                        <oauth-login-component
-                            v-if="enableGoogleLogin"
-                            provider="google"
-                            image="google.png"
-                            button-text="Login with Google"
-                            class="
-                                border-l-4 border-solid
-                                p-4
-                                cursor-pointer
-                                hover:border-yellow-400 hover:bg-yellow-100
-                                focus:ring-2 focus:ring-yellow-100
-                            "
-                        />
-                    </span>
+                    <oauth-login-component
+                        v-for="provider of loginProviders"
+                        :key="provider.name"
+                        :provider="provider.name"
+                        :image="provider.icon"
+                        :button-text="provider.text"
+                        class="
+                            border-l-4 border-solid
+                            p-4
+                            cursor-pointer
+                            hover:border-yellow-400 hover:bg-yellow-100
+                            focus:ring-2 focus:ring-yellow-100
+                        "
+                    />
                 </div>
             </div>
         </div>
@@ -59,8 +39,6 @@
 
 <script>
 import { defineAsyncComponent } from "vue";
-import HTTPService from "@/http.service";
-const httpService = new HTTPService();
 
 export default {
     components: {
@@ -71,8 +49,10 @@ export default {
     data() {
         return {
             siteName: this.$store.state.configuration.ui.siteName,
-            recaptchaSiteKey: this.$store.state.configuration.ui.recaptcha.siteKey,
-            enableAuthenticationMethods: true,
+            loginProviders: [
+                { name: "aaf", icon: "aaf.png", text: "Login with the AAF" },
+                { name: "google", icon: "google.png", text: "Login with Google" },
+            ],
         };
     },
     computed: {
@@ -82,23 +62,6 @@ export default {
         enableAafLogin() {
             return this.$store.state.configuration.authentication.includes("aaf");
         },
-    },
-    mounted() {
-        // this.initRecaptcha();
-    },
-    methods: {
-        // initRecaptcha() {
-        //     grecaptcha.render(this.$refs.recaptcha, {
-        //         sitekey: this.recaptchaSiteKey,
-        //         callback: this.handleRecaptcha,
-        //     });
-        // },
-        // async handleRecaptcha(token) {
-        //     let response = await httpService.post({ route: "/recaptcha", body: { token } });
-        //     if (response.status === 200) {
-        //         this.enableAuthenticationMethods = true;
-        //     }
-        // },
     },
 };
 </script>

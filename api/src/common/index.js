04673046ed1e8fde4ec4e1cfeb17677580efee53
Marco La Rosa <m@lr.id.au>
2021-10-18 14:12:14 +1100

    implement method to get a tmp dir for operations

--- api/src/common/index.js
+++ api/src/common/index.js
@@ -1,3 +1,5 @@
+import path from "path";
+import { ensureDir } from "fs-extra";
 export { loadConfiguration, filterPrivateInformation } from "./configuration";
 export { getLogger } from "./logger";
 export { getS3Handle } from "./getS3Handle";
@@ -10,3 +12,12 @@ export {
     teardownAfterAll,
     teardownAfterEach,
 } from "./test-utils";
+
+export async function getUserTempLocation({ userId }) {
+    if (!userId) {
+        throw new Error(`'userId' must be provided`);
+    }
+    let tempdir = path.join("/srv", "tmp", userId);
+    await ensureDir(tempdir);
+    return tempdir;
+}

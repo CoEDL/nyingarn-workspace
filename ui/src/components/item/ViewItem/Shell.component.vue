c79dd0491fe206893a760364cdab4e9b30bd5ea0
Marco La Rosa <m@lr.id.au>
2022-01-14 13:55:32 +1100

    style section heights

--- ui/src/components/item/ViewItem/Shell.component.vue
+++ ui/src/components/item/ViewItem/Shell.component.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col h-screen">
         <div class="text-xl mb-2 bg-indigo-200 p-4">
             {{ identifier }}
         </div>

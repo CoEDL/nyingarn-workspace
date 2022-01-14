3e28e9d67ca8f63314ff077ad053794b4c47aef3
Marco La Rosa <m@lr.id.au>
2022-01-14 13:09:04 +1100

    implement ftp processing method w/ test

--- tasks/src/tasks/transcription-processing.js
+++ tasks/src/tasks/transcription-processing.js
@@ -1,9 +1,11 @@
 import path from "path";
-import { pathExists, createReadStream, writeFile } from "fs-extra";
+import { readFile, pathExists, createReadStream, writeFile, appendFile } from "fs-extra";
 import { parse } from "csv-parse";
-import { zipObject } from "lodash";
+import { zipObject, isEmpty } from "lodash";
 import { getS3Handle } from "../common";
 import { getFiles } from "./";
+let { DOMParser } = require("xmldom");
+import xmlserializer from "xmlserializer";
 
 export async function processDigivolTranscription({ directory, identifier, resource }) {
     let file = path.join(directory, identifier, resource);
@@ -35,10 +37,61 @@ export async function processDigivolTranscription({ directory, identifier, resou
 
             if (!exists) {
                 filename = path.join(directory, identifier, filename);
+                await writeFile(filename, `<pb facs="${record.externalIdentifier}"/>`);
                 await writeFile(filename, record.occurrenceRemarks);
             }
         }
     }
 }
 
-export async function processFtpTeiTranscription({ directory, identifier, resource }) {}
+export async function processFtpTeiTranscription({ directory, identifier, resource }) {
+    const dom = new DOMParser({
+        locator: {},
+        errorHandler: {
+            warning: () => {},
+            error: (e) => console.log("ERROR:", file, e.replace("\n", " - ")),
+            fatalError: (e) => console.log(file, e),
+        },
+    });
+    const file = path.join(directory, identifier, resource);
+    let data = await readFile(file);
+    const doc = dom.parseFromString(data.toString());
+
+    let nodes = getElementsByName(doc, "pb");
+    let pbIds = nodes.map((n) => n.getAttribute("xml:id"));
+    let divs = getElementsByName(doc, "div");
+
+    let { bucket } = await getS3Handle();
+    for (let pbId of pbIds) {
+        const id = pbId.replace("F", "OTP");
+        let node = divs.filter((n) => n.getAttribute("xml:id") === id)[0];
+
+        let paragraphs = getElementsByName(node, "p");
+
+        let pageIdentifier = getElementsByName(node, "fw")[0].childNodes[0].nodeValue;
+
+        let filename = `${pageIdentifier}.tei.xml`;
+        let teiFile = path.join(identifier, filename);
+        let exists = await bucket.pathExists({ path: teiFile });
+
+        if (!exists) {
+            filename = path.join(directory, identifier, filename);
+            await writeFile(filename, `<pb facs="${pageIdentifier}"/>\n`);
+            for (let p of paragraphs) {
+                p.removeAttribute("xml:id");
+                await appendFile(
+                    filename,
+                    xmlserializer
+                        .serializeToString(p)
+                        .replace(' xmlns="http://www.tei-c.org/ns/1.0"', "")
+                );
+            }
+        }
+    }
+}
+
+function getElementsByName(node, element) {
+    let nodes = node.getElementsByTagName(element);
+    let keys = Object.keys(nodes).filter((k) => k.match(/^\d+$/));
+    return keys.map((k) => nodes[k]);
+}

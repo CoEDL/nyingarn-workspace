eb70f9a0f5df526dba1ce5a39e6b1265f42225f8
Marco La Rosa <m@lr.id.au>
2022-01-17 13:13:49 +1100

    placeholder styling

--- ui/src/components/item/ViewItem/ViewItemContent/DisplayImageThumbnail.component.vue
+++ ui/src/components/item/ViewItem/ViewItemContent/DisplayImageThumbnail.component.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <el-image :src="src">
-            <template #error
-                ><div class="h-64 w-40 bg-gray-100 text-sm p-2 text-center">
+            <template #error>
+                <div class="h-40 bg-gray-100 text-sm p-2 text-center">
                     thumbnail not yet available
-                </div></template
-            >
+                </div>
+            </template>
         </el-image>
     </div>
 </template>

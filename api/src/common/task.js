ef87247c09813228eaeae28a80b7ccb65f3d6153
Marco La Rosa <m@lr.id.au>
2022-02-09 09:18:54 +1100

    store resource being operated on as part of task

--- api/src/common/task.js
+++ api/src/common/task.js
@@ -1,7 +1,7 @@
 import { loadConfiguration } from ".";
 import models from "../models";
 
-export async function registerTask({ itemId, status, name, data }) {
+export async function registerTask({ itemId, status, resource, name, data }) {
     const statuses = ["in progress", "done", "failed"];
     if (!status || !statuses.includes(status)) {
         throw new Error(`'status' is required and must be one of '${statuses}'`);
@@ -10,7 +10,7 @@ export async function registerTask({ itemId, status, name, data }) {
         throw new Error(`'itemId' is required`);
     }
     try {
-        return (await models.task.create({ itemId, status, name, data })).get();
+        return (await models.task.create({ itemId, status, resource, name, data })).get();
     } catch (error) {
         console.log(error);
         log.error(`Couldn't update tasks table: ${status}: ${text}`);
@@ -21,6 +21,7 @@ export async function submitTask({ item, name, body }) {
     let configuration = await loadConfiguration();
     let task = await registerTask({
         itemId: item.id,
+        resource: body.resource,
         status: "in progress",
         name,
         data: { resource: body.resource },

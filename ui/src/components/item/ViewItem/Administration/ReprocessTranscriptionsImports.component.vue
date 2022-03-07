604aa3c1e09b8d5e1722ed66dd831e9c0237799e
Marco La Rosa <m@lr.id.au>
2022-03-08 10:48:21 +1100

    disable reprocess button on submit; show message saying it's done

--- ui/src/components/item/ViewItem/Administration/ReprocessTranscriptionsImports.component.vue
+++ ui/src/components/item/ViewItem/Administration/ReprocessTranscriptionsImports.component.vue
@@ -5,21 +5,26 @@
             TEI transcription.
         </div>
         <div>
-            <el-button @click="reprocessImports">Reprocess transcription files</el-button>
+            <el-button @click="reprocessImports" :disabled="disabled">
+                Reprocess transcription files
+            </el-button>
         </div>
     </div>
 </template>
 
 <script>
+import { ElMessage } from "element-plus";
 import { reprocessImports } from "@/components/item/load-item-data";
 export default {
     data() {
         return {
             identifier: this.$route.params.identifier,
+            disabled: false,
         };
     },
     methods: {
         async reprocessImports() {
+            this.disabled = true;
             let response = await reprocessImports({
                 $http: this.$http,
                 identifier: this.identifier,
@@ -29,6 +34,10 @@ export default {
             for (let file of imports) {
                 await this.$http.post({ route: `/process/post-finish/${this.identifier}/${file}` });
             }
+            ElMessage({
+                message: "Reprocessing tasks have been submitted. They should complete soon.",
+                type: "success",
+            });
         },
     },
 };

9e36b97f4f9e55888ae7c1b96376bf943c61abbf
Marco La Rosa <m@lr.id.au>
2022-05-02 14:10:59 +1000

    convert to composition api

--- ui/src/components/LinkRelatedResource.component.vue
+++ ui/src/components/LinkRelatedResource.component.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col">
-        <el-table :data="items">
+        <el-table :data="data.items">
             <template #empty>You have no items. Get started by creating an item.</template>
             <el-table-column prop="name" label="Name"> </el-table-column>
             <el-table-column prop="type" label="Type" width="150"> </el-table-column>
@@ -9,7 +9,7 @@
                     <el-button
                         type="primary"
                         @click="linkItemToCollection(scope.row)"
-                        :disabled="loading"
+                        :disabled="data.loading"
                     >
                         <i class="fa-solid fa-link"></i>
                     </el-button>
@@ -19,129 +19,126 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import { ElMessage } from "element-plus";
-export default {
-    props: {
-        link: {
-            type: String,
-            required: true,
-            validator: function (value) {
-                return ["collections", "items", "all"].includes(value);
-            },
-        },
-        property: {
-            type: String,
-            required: true,
-            validator: function (value) {
-                return ["hasMember", "memberOf"].includes(value);
-            },
-        },
-    },
-    data() {
-        return {
-            loading: false,
-            identifier: this.$route.params.identifier,
-            items: [],
-        };
-    },
-    async mounted() {
-        if (this.link === "items") {
-            let items = await this.getMyItems();
-            this.items = [...items];
-        } else if (this.link === "collections") {
-            let collections = await this.getMyCollections();
-            this.items = [...collections];
-        } else {
-            let items = await this.getMyItems();
-            let collections = await this.getMyCollections();
-            this.items = [...items, ...collections];
-        }
-    },
-    methods: {
-        async getMyItems() {
-            let response = await this.$http.get({
-                route: `/items`,
-            });
-            if (response.status === 200) {
-                response = await response.json();
-                let items = response.items.map((i) => ({ name: i, type: "item" }));
-                if (this.$route.path.match("/items")) {
-                    items = items.filter((c) => c.name !== this.identifier);
-                }
-                return items;
-            }
-        },
-        async getMyCollections() {
-            let response = await this.$http.get({
-                route: `/collections`,
-            });
-            if (response.status === 200) {
-                response = await response.json();
-                let collections = response.collections.map((i) => ({
-                    name: i,
-                    type: "collection",
-                }));
-                if (this.$route.path.match("/collections")) {
-                    collections = collections.filter((c) => c.name !== this.identifier);
-                }
-                return collections;
-            }
-        },
-        async linkItemToCollection(item) {
-            this.loading = true;
-            let reverseProperty = this.property === "hasMember" ? "memberOf" : "hasMember";
+import { reactive, onMounted, inject } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const $http = inject("$http");
 
-            let updates = {
-                source: {
-                    identifier: this.identifier,
-                    entities: [
-                        {
-                            "@id": "./",
-                            "@type": "Dataset",
-                            [this.property]: [{ "@id": `arcp://name,/nyingarn.net/${item.name}` }],
-                        },
-                        {
-                            "@id": `arcp://name,/nyingarn.net/${item.name}`,
-                            "@type": "URL",
-                        },
-                    ],
-                },
-                target: {
-                    identifier: item.name,
-                    entities: [
-                        {
-                            "@id": "./",
-                            "@type": "Dataset",
-                            [reverseProperty]: [
-                                { "@id": `arcp://name,/nyingarn.net/${this.identifier}` },
-                            ],
-                        },
-                        {
-                            "@id": `arcp://name,/nyingarn.net/${this.identifier}`,
-                            "@type": "URL",
-                        },
-                    ],
-                },
-            };
-            let response = await this.$http.post({
-                route: "/describo/update",
-                body: { updates },
-            });
-            if (response.status === 200) {
-                ElMessage({
-                    message: "item linked",
-                    type: "success",
-                });
-            } else {
-                ElMessage({
-                    message: "There was an issue creating the item link",
-                    type: "error",
-                });
-                console.error(await response.json());
-            }
-            this.loading = false;
+const props = defineProps({
+    link: {
+        type: String,
+        required: true,
+        validator: function (value) {
+            return ["collections", "items", "all"].includes(value);
         },
     },
-};
+    property: {
+        type: String,
+        required: true,
+        validator: function (value) {
+            return ["hasMember", "memberOf"].includes(value);
+        },
+    },
+});
+const data = reactive({
+    loading: false,
+    identifier: route.params.identifier,
+    items: [],
+});
+onMounted(async () => {
+    if (props.link === "items") {
+        let items = await getMyItems();
+        data.items = [...items];
+    } else if (props.link === "collections") {
+        let collections = await getMyCollections();
+        data.items = [...collections];
+    } else {
+        let items = await getMyItems();
+        let collections = await getMyCollections();
+        data.items = [...items, ...collections];
+    }
+});
+async function getMyItems() {
+    let response = await $http.get({
+        route: `/items`,
+    });
+    if (response.status === 200) {
+        response = await response.json();
+        let items = response.items.map((i) => ({ name: i, type: "item" }));
+        if (route.path.match("/items")) {
+            items = items.filter((c) => c.name !== data.identifier);
+        }
+        return items;
+    }
+}
+async function getMyCollections() {
+    let response = await $http.get({
+        route: `/collections`,
+    });
+    if (response.status === 200) {
+        response = await response.json();
+        let collections = response.collections.map((i) => ({
+            name: i,
+            type: "collection",
+        }));
+        if (route.path.match("/collections")) {
+            collections = collections.filter((c) => c.name !== data.identifier);
+        }
+        return collections;
+    }
+}
+async function linkItemToCollection(item) {
+    data.loading = true;
+    let reverseProperty = props.property === "hasMember" ? "memberOf" : "hasMember";
+
+    let updates = {
+        source: {
+            identifier: data.identifier,
+            entities: [
+                {
+                    "@id": "./",
+                    "@type": "Dataset",
+                    [props.property]: [{ "@id": `arcp://name,/nyingarn.net/${item.name}` }],
+                },
+                {
+                    "@id": `arcp://name,/nyingarn.net/${item.name}`,
+                    "@type": "URL",
+                },
+            ],
+        },
+        target: {
+            identifier: item.name,
+            entities: [
+                {
+                    "@id": "./",
+                    "@type": "Dataset",
+                    [reverseProperty]: [{ "@id": `arcp://name,/nyingarn.net/${data.identifier}` }],
+                },
+                {
+                    "@id": `arcp://name,/nyingarn.net/${data.identifier}`,
+                    "@type": "URL",
+                },
+            ],
+        },
+    };
+    let response = await $http.post({
+        route: "/describo/update",
+        body: { updates },
+    });
+    if (response.status === 200) {
+        ElMessage({
+            message: "item linked",
+            type: "success",
+        });
+    } else {
+        ElMessage({
+            message: "There was an issue creating the item link",
+            type: "error",
+        });
+        console.error(await response.json());
+    }
+    data.loading = false;
+}
 </script>
--- ui/src/components/collection/ViewCollection/MetadataComponent/Shell.component.vue
+++ ui/src/components/collection/ViewCollection/MetadataComponent/Shell.component.vue
@@ -1,46 +1,53 @@
 <template>
     <div>
-	<iframe :src="url" class="h-full" :style="frameStyle" />
+        <iframe :src="data.url" class="h-full" :style="frameStyle" />
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-	return {
-	    url: undefined,
-	    windowWidth: window.innerWidth,
-	};
-    },
-    computed: {
-	frameStyle: function () {
-	    if (this.windowWidth <= 1100) {
-		return {
-		    width: `${this.windowWidth * (4 / 6) - 60}px`,
-		    height: `${window.innerHeight - 160}px`,
-		};
-	    } else {
-		return {
-		    width: `${this.windowWidth * (5 / 6) - 40}px`,
-		    height: `${window.innerHeight - 160}px`,
-		};
-	    }
-	},
-    },
-    mounted() {
-	window.addEventListener("resize", () => {
-	    this.windowWidth = window.innerWidth;
-	});
-	this.init();
-    },
-    methods: {
-	async init() {
-	    let response = await this.$http.post({
-		route: "/describo",
-		body: { folder: this.$route.params.identifier },
-	    });
-	    this.url = (await response.json()).url;
-	},
-    },
-};
+<script setup>
+import { reactive, computed, onMounted, inject } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const $http = inject("$http");
+import { ElLoading } from "element-plus";
+
+const data = reactive({
+    url: undefined,
+    loading: true,
+    windowWidth: window.innerWidth,
+});
+
+let frameStyle = computed(() => {
+    if (data.windowWidth <= 1100) {
+        return {
+            width: `${data.windowWidth * (4 / 6) - 60}px`,
+            height: `${window.innerHeight - 160}px`,
+        };
+    } else {
+        return {
+            width: `${data.windowWidth * (5 / 6) - 40}px`,
+            height: `${window.innerHeight - 160}px`,
+        };
+    }
+});
+onMounted(() => {
+    window.addEventListener("resize", () => {
+        this.windowWidth = window.innerWidth;
+    });
+    init();
+});
+async function init() {
+    let loading = ElLoading.service({
+        lock: true,
+        text: "Loading",
+        background: "rgba(0, 0, 0, 0.7)",
+    });
+    let response = await $http.post({
+        route: "/describo",
+        body: { folder: route.params.identifier },
+    });
+    data.url = (await response.json()).url;
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    loading.close();
+}
 </script>

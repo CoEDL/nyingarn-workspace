a943aa0f0b2a063c0f8f7b5866ade35d4bc134f7
Marco La Rosa <m@lr.id.au>
2022-06-03 10:20:44 +1000

    implement crate mgr class w/ tests

--- ui/src/components/describo-lite/helper-methods.js
+++ ui/src/components/describo-lite/helper-methods.js
@@ -1,43 +1,5 @@
 import { v4 as uuid } from "uuid";
-
-export function getRootDataset({ crate }) {
-    let rootDescriptor = crate.filter(
-        (e) => e["@id"] === "ro-crate-metadata.json" && e["@type"] === "CreativeWork"
-    )[0];
-    return crate.filter((e) => e["@id"] === rootDescriptor.about["@id"])[0];
-}
-
-export function convertCrateToDatabase({ crate }) {
-    let entities = [];
-    let properties = [];
-
-    // assign describoId to all entities
-    let graph = crate["@graph"].map((e) => ({ describoId: uuid(), ...e }));
-
-    // mark root dataset
-    let rootDataset = getRootDataset({ crate: graph });
-    graph = graph.map((e) => {
-        if (e["@id"] === rootDataset["@id"]) {
-            return { describoLabel: "RootDataset", ...e };
-        } else {
-            return e;
-        }
-    });
-
-    // filter out root descriptor
-    let rootDescriptor;
-    graph = graph.filter(
-        (e) => !(e["@id"] === "ro-crate-metadata.json" && e["@type"] === "CreativeWork")
-    );
-
-    console.log(graph);
-    // filter out root descriptor
-    // for (let entity of crate["@graph"]) {
-    //     for (let property of Object.keys(entity)) {
-    //         console.log(property, JSON.stringify(entity[property]));
-    //     }
-    // }
-}
+import { isString, isArray, isPlainObject, groupBy, cloneDeep } from "lodash";
 
 export function getProfileDefinitionForProperty({ profile, entityType, property }) {
     let entityDefinition = profile.classes[entityType];
@@ -45,26 +7,23 @@ export function getProfileDefinitionForProperty({ profile, entityType, property 
     return propertyDefinition;
 }
 
-export function saveProperty({ crate, entity, data }) {
-    console.debug("saveProperty", JSON.stringify(data, null, 2));
-    // find the relevant entity in the crate
-    let updatedEntity = data.crate.filter((e) => e.describoId === data.entity.describoId)[0];
-    updatedEntity[data.property];
-}
-
 export class CrateManager {
     constructor({ crate }) {
         this.context = crate["@context"];
 
+        // store root descriptor as found
+        this.rootDescriptor = crate["@graph"].filter(
+            (e) => e["@id"] === "ro-crate-metadata.json" && e["@type"] === "CreativeWork"
+        )[0];
         // filter root descriptor from the graph
         this.graph = crate["@graph"].filter(
             (e) => !(e["@id"] === "ro-crate-metadata.json" && e["@type"] === "CreativeWork")
         );
 
-        // but store it for later
-        this.rootDescriptor = crate["@graph"].filter(
-            (e) => e["@id"] === "ro-crate-metadata.json" && e["@type"] === "CreativeWork"
-        )[0];
+        this.describoProperties = ["describoId", "describoLabel"];
+        this.coreProperties = ["describoId", "describoLabel", "@id", "@type", "name"];
+        this.entities = [];
+        this.properties = [];
     }
 
     init() {
@@ -79,6 +38,128 @@ export class CrateManager {
                 : e;
         });
 
-        console.log(this.graph);
+        // for each entity, populate entities and properties structs
+        this.graph.forEach((e) => {
+            let entity = this.coreProperties
+                .map((p) => ({ [p]: e[p] }))
+                .reduce((obj, entry) => ({ ...obj, ...entry }));
+            this.entities.push(entity);
+        });
+        this.entitiesByAtId = groupBy(this.entities, "@id");
+        this.entitiesByType = groupBy(this.entities, "@type");
+        this.entitiesByDescriboId = groupBy(this.entities, "describoId");
+
+        this.graph.forEach((e) => {
+            this.__processProperties({ entity: e });
+        });
+    }
+
+    exportCrate() {
+        let crate = {
+            "@context": cloneDeep(this.context),
+            "@graph": [cloneDeep(this.rootDescriptor)],
+        };
+        this.entities.forEach((entity) => {
+            // map in the entity properties
+            let properties = this.properties.filter((p) => p.entityId === entity.describoId);
+            properties = groupBy(properties, "property");
+            for (let property of Object.keys(properties)) {
+                entity[property] = [];
+                properties[property].forEach((instance) => {
+                    if (instance.tgtEntityId) {
+                        let targetEntity = this.__lookupEntityByDescriboId({
+                            id: instance.tgtEntityId,
+                        });
+                        entity[property].push({
+                            "@id": targetEntity ? targetEntity["@id"] : instance.value,
+                        });
+                    } else {
+                        entity[property].push(instance.value);
+                    }
+                });
+                if (entity[property].length === 1) entity[property] = entity[property][0];
+            }
+
+            // map in the reverse property links
+            entity["@reverse"] = {};
+            let reverseProperties = this.properties.filter(
+                (p) => p.tgtEntityId === entity.describoId
+            );
+            reverseProperties = groupBy(reverseProperties, "property");
+            for (let property of Object.keys(reverseProperties)) {
+                entity["@reverse"][property] = [];
+                reverseProperties[property].forEach((instance) => {
+                    let referencedEntity = this.entitiesByDescriboId[instance.entityId][0];
+                    entity["@reverse"][property].push({ "@id": referencedEntity["@id"] });
+                });
+                if (entity["@reverse"][property].length === 1)
+                    entity["@reverse"][property] = entity["@reverse"][property][0];
+            }
+
+            this.describoProperties.forEach((p) => delete entity[p]);
+            crate["@graph"].push(entity);
+        });
+        return crate;
+    }
+
+    getRootDataset() {
+        return this.entities.filter((e) => e.describoLabel === "RootDataset")[0];
+    }
+
+    __processProperties({ entity }) {
+        const pushProperty = this.__pushProperty.bind(this);
+
+        for (let prop of Object.keys(entity)) {
+            if (this.coreProperties.includes(prop)) continue;
+
+            if (isString(entity[prop])) {
+                pushProperty({
+                    entityId: entity.describoId,
+                    propertyId: uuid(),
+                    property: prop,
+                    value: entity[prop],
+                });
+            } else if (isPlainObject(entity[prop])) {
+                entity[prop] = [entity[prop]];
+            }
+
+            if (isArray(entity[prop])) {
+                entity[prop].forEach((instance) => {
+                    if (isString(instance)) {
+                        pushProperty({
+                            entityId: entity.describoId,
+                            propertyId: uuid(),
+                            property: prop,
+                            value: instance,
+                        });
+                    } else if (isPlainObject(instance)) {
+                        if ("@id" in instance) {
+                            let targetEntity = this.__lookupEntityByAtId({ id: instance["@id"] });
+                            pushProperty({
+                                entityId: entity.describoId,
+                                propertyId: uuid(),
+                                property: prop,
+                                value: instance["@id"],
+                                tgtEntityId: targetEntity?.describoId || "not found",
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    __pushProperty({ entityId, propertyId, property, value, tgtEntityId }) {
+        this.properties.push({ entityId, propertyId, property, value, tgtEntityId });
+    }
+
+    __lookupEntityByAtId({ id }) {
+        let targetEntity = cloneDeep(this.entitiesByAtId[id]);
+        if (targetEntity?.length) return targetEntity.shift();
+    }
+
+    __lookupEntityByDescriboId({ id }) {
+        let targetEntity = cloneDeep(this.entitiesByDescriboId[id]);
+        if (targetEntity?.length) return targetEntity.shift();
     }
 }
--- ui/src/components/describo-lite/helper-methods.spec.js
+++ ui/src/components/describo-lite/helper-methods.spec.js
@@ -1,36 +1,269 @@
 import "regenerator-runtime";
 import { CrateManager } from "./helper-methods";
+import { faker } from "@faker-js/faker";
+import { range, round, compact, groupBy, random } from "lodash";
+import { performance } from "perf_hooks";
 
-describe("Test loading crate files and converting to a JSON DB", () => {
-    it("should be able to load in a basic crate file", () => {
-        let crate = getBasicCrate();
+describe("Test loading / exporting crate files", () => {
+    test("base crate file", async () => {
+        let crate = getBaseCrate();
 
         let crateManager = new CrateManager({ crate });
         crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        expect(crate).toEqual(exportedCrate);
+    });
+    test("with root dataset, one type", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset"],
+            name: "Dataset",
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        exportedCrate["@graph"] = exportedCrate["@graph"].map((e) => {
+            delete e["@reverse"];
+            return e;
+        });
+        expect(crate).toEqual(exportedCrate);
+    });
+    test("with root dataset, multiple types", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset", "Something Else"],
+            name: "Dataset",
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        exportedCrate["@graph"] = exportedCrate["@graph"].map((e) => {
+            delete e["@reverse"];
+            return e;
+        });
+        expect(crate).toEqual(exportedCrate);
+    });
+    test("with root dataset and one text property", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset"],
+            name: "Dataset",
+            text: "some text",
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        exportedCrate["@graph"] = exportedCrate["@graph"].map((e) => {
+            delete e["@reverse"];
+            return e;
+        });
+        expect(crate).toEqual(exportedCrate);
+    });
+    test("with root dataset and one text property in array", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset"],
+            name: "Dataset",
+            text: ["some text"],
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        let rootDataset = exportedCrate["@graph"].filter((e) => e["@id"] === "./");
+        expect(rootDataset).toEqual([
+            {
+                "@id": "./",
+                "@type": ["Dataset"],
+                name: "Dataset",
+                text: "some text",
+                "@reverse": {},
+            },
+        ]);
+    });
+    test("with root dataset and one reference to another object", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset"],
+            name: "Dataset",
+            author: { "@id": "http://entity.com/something" },
+        });
+        crate["@graph"].push({
+            "@id": "http://entity.com/something",
+            "@type": "Person",
+            name: "Person",
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        let rootDataset = exportedCrate["@graph"].filter((e) => e["@id"] === "./");
+        expect(rootDataset).toEqual([
+            {
+                "@id": "./",
+                "@type": ["Dataset"],
+                name: "Dataset",
+                author: { "@id": "http://entity.com/something" },
+                "@reverse": {},
+            },
+        ]);
+
+        let entity = exportedCrate["@graph"].filter(
+            (e) => e["@id"] === "http://entity.com/something"
+        );
+        expect(entity).toEqual([
+            {
+                "@id": "http://entity.com/something",
+                "@type": "Person",
+                name: "Person",
+                "@reverse": { author: { "@id": "./" } },
+            },
+        ]);
+    });
+    test("with root dataset and one reference to object not in crate", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset"],
+            name: "Dataset",
+            author: { "@id": "http://entity.com/something" },
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        let rootDataset = exportedCrate["@graph"].filter((e) => e["@id"] === "./");
+        expect(rootDataset).toEqual([
+            {
+                "@id": "./",
+                "@type": ["Dataset"],
+                name: "Dataset",
+                author: { "@id": "http://entity.com/something" },
+                "@reverse": {},
+            },
+        ]);
+
+        let entity = exportedCrate["@graph"].filter(
+            (e) => e["@id"] === "http://entity.com/something"
+        );
+        expect(entity).toEqual([]);
+    });
+    test("with root dataset and mixed type values for a property", async () => {
+        let crate = getBaseCrate();
+        crate["@graph"].push({
+            "@id": "./",
+            "@type": ["Dataset"],
+            name: "Dataset",
+            author: [{ "@id": "http://entity.com/something" }, "some text"],
+        });
+        crate["@graph"].push({
+            "@id": "http://entity.com/something",
+            "@type": "Person",
+            name: "Person",
+        });
+
+        let crateManager = new CrateManager({ crate });
+        crateManager.init();
+
+        let exportedCrate = crateManager.exportCrate();
+        let rootDataset = exportedCrate["@graph"].filter((e) => e["@id"] === "./");
+        expect(rootDataset[0].author).toEqual([
+            {
+                "@id": "http://entity.com/something",
+            },
+            "some text",
+        ]);
     });
 });
 
-function getBasicCrate() {
-    let context = ["https://w3id.org/ro/crate/1.1/context"];
-    let graph = [
-        {
-            "@id": "ro-crate-metadata.json",
-            "@type": "CreativeWork",
-            conformsTo: {
-                "@id": "https://w3id.org/ro/crate/1.1/context",
-            },
-            about: {
+describe.skip("Test loading large crates and see how it performs", () => {
+    test("n = 10, 100, 500, 1000, 2000, 4000, 8000, 16000", async () => {
+        const tests = [10, 100, 500, 1000, 2000, 4000, 8000, 16000];
+        // const tests = [2];
+        for (const total of tests) {
+            let crate = getBaseCrate();
+            crate["@graph"].push({
                 "@id": "./",
-            },
-        },
-        {
-            "@id": "./",
-            "@type": "Dataset",
-            name: "test crate",
-        },
-    ];
+                "@type": ["Dataset"],
+                name: "Dataset",
+            });
+
+            let entities = crate["@graph"].map((e) => {
+                return e?.about?.["@id"] === "./" ? null : e;
+            });
+            entities = compact(entities);
+            const runtime = {};
+
+            let t0 = performance.now();
+            for (let i in range(total)) {
+                let nEntities = entities.length;
+                let pick = entities[random(0, nEntities - 1)];
+
+                let entity = {
+                    "@id": faker.internet.url(),
+                    "@type": faker.helpers.arrayElement([
+                        "Dataset",
+                        "File",
+                        "Person",
+                        "Organisation",
+                    ]),
+                    name: faker.commerce.productDescription(),
+                    [faker.word.noun()]: [{ "@id": pick["@id"] }],
+                };
+                crate["@graph"].push(entity);
+                entities.push(entity);
+            }
+
+            let t1 = performance.now();
+            runtime.generate = round(t1 - t0, 2);
+
+            t0 = performance.now();
+            let crateManager = new CrateManager({ crate });
+            crateManager.init();
+            t1 = performance.now();
+            runtime.init = round(t1 - t0, 2);
+
+            t0 = performance.now();
+            let exportedCrate = crateManager.exportCrate();
+            t1 = performance.now();
+            runtime.export = round(t1 - t0, 2);
+
+            console.log(
+                `N items in crate: ${crate["@graph"].length}, generate: ${runtime.generate}ms, init: ${runtime.init}ms, export: ${runtime.export}ms`
+            );
+        }
+    });
+});
+
+function getBaseCrate() {
     return {
-        "@context": context,
-        "@graph": graph,
+        "@context": ["https://w3id.org/ro/crate/1.1/context"],
+        "@graph": [
+            {
+                "@id": "ro-crate-metadata.json",
+                "@type": "CreativeWork",
+                conformsTo: {
+                    "@id": "https://w3id.org/ro/crate/1.1/context",
+                },
+                about: {
+                    "@id": "./",
+                },
+            },
+        ],
     };
 }

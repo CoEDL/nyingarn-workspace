a8057279958de34fe72c6f488ef367864098f775
Marco La Rosa <m@lr.id.au>
2022-09-15 16:30:19 +1000

    add lookup class for crate builder

---
+++ ui/src/components/item/ViewItem/lookup-templates.js
@@ -0,0 +1,25 @@
+export class Lookup {
+    constructor({ templates }) {
+        this.templates = templates;
+    }
+    entityTemplates({ type, filter, limit = 5 }) {
+        let templates = this.templates
+            .filter((template) => template.entity)
+            .filter((template) => {
+                if (type && !filter) {
+                    return template.entity["@type"] === type;
+                } else if (type && filter) {
+                    return (
+                        template.entity["@type"] === type &&
+                        (template.entity["@id"].match(filter) || template.entity.name.match(filter))
+                    );
+                } else {
+                    return template;
+                }
+            });
+        return templates.slice(0, limit);
+    }
+    crateTemplates() {
+        return this.templates.filter((template) => template.crate);
+    }
+}

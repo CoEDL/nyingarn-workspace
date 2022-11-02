809e0554d7182b2b3f35ff2bd45027243b06d415
Conal Tuohy <conal.tuohy@gmail.com>
2022-11-02 14:23:13 +1000

    restructure XSLT compilation

--- tasks/src/xslt/compile.sh
+++ tasks/src/xslt/compile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # compiles the main process-tei.xsl into SaxonJS's JSON-based executable format
-for stylesheet in /srv/tasks/src/xslt/*.xsl; do
+SOURCE_DIR=/srv/tasks/src/xslt
+for stylesheet in $SOURCE_DIR/*.xsl; do
     node /srv/tasks/node_modules/xslt3 -t -xsl:"$stylesheet" -export:"$stylesheet.sef.json" -nogo
 done
+# move front end compiled XSLT
+mv $SOURCE_DIR/tei-to-html.xsl.sef.json /srv/api/src/common/

6a5a90bd3df83146bda1c14b71f5e6b1b93993c8
Marco La Rosa <m@lr.id.au>
2022-12-02 11:14:00 +1100

    revise so that enter submits - closes #128

--- ui/src/components/collection/CreateCollection.component.vue
+++ ui/src/components/collection/CreateCollection.component.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div @click="createNewCollection = !createNewCollection" class="cursor-pointer">
+        <div @click="data.createNewCollection = !data.createNewCollection" class="cursor-pointer">
             <span class="fa-layers fa-fw">
                 <i class="fa-solid fa-file-image" data-fa-transform="left-4 up-4"></i>
                 <i class="fa-solid fa-file-image" data-fa-transform="right-4 down-4"></i>
@@ -9,85 +9,95 @@
         </div>
         <el-dialog
             title="Create a new Collection"
-            v-model="createNewCollection"
+            v-model="data.createNewCollection"
             width="80%"
             destroy-on-close
         >
-            <div class="text-gray-600">
-                <div class="flex flex-col space-y-2">
-                    <div class="flex flex-row">
-                        <div class="text-sm text-gray-600">
-                            Please define an identifier for this collection.
-                        </div>
-                        <div class="flex-grow"></div>
-                        <div v-if="error?.code" class="text-red-800">
-                            Error: {{ error.message }}
-                        </div>
+            <div class="flex flex-col space-y-2 text-gray-600" v-loading="data.loading">
+                <div class="flex flex-row">
+                    <div class="text-sm text-gray-600">
+                        Please define an identifier for this collection.
                     </div>
-                    <div class="w-full">
-                        <el-input
-                            placeholder="collection identifier"
-                            v-model="identifier"
-                            @input="checkNameStructure"
-                        ></el-input>
+                    <div class="flex-grow"></div>
+                    <div v-if="data.error?.code" class="text-red-800">
+                        Error: {{ data.error.message }}
                     </div>
-                    <div class="text-xs text-gray-600" :class="{ 'text-red-600': error }">
-                        {{ help }}
-                    </div>
-                    <div>
-                        <el-button
-                            type="primary"
-                            @click="createCollectionEntry"
-                            :disabled="!identifier.length || error"
-                        >
-                            Create this collection
-                        </el-button>
-                    </div>
+                </div>
+                <div class="w-full" v-loading="data.loading">
+                    <el-form
+                        :model="data.form"
+                        label-width="120px"
+                        @submit.prevent="createCollectionEntry"
+                    >
+                        <el-form-item label="Collection" required>
+                            <el-input
+                                :autofocus="true"
+                                placeholder="collection identifier"
+                                v-model="data.identifier"
+                                @input="checkNameStructure"
+                            ></el-input>
+                            <div
+                                class="text-xs text-gray-600"
+                                :class="{ 'text-red-600': data.error }"
+                            >
+                                {{ data.help }}
+                            </div>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button
+                                type="primary"
+                                @click="createCollectionEntry"
+                                :disabled="!data.identifier.length || data.error"
+                            >
+                                Create this collection
+                            </el-button>
+                        </el-form-item>
+                    </el-form>
                 </div>
             </div>
         </el-dialog>
     </div>
 </template>
 
-<script>
+<script setup>
 import { createCollection } from "./collection-services";
-export default {
-    data() {
-        return {
-            identifier: "",
-            createNewCollection: false,
-            error: false,
-            help: this.$store.state.configuration.ui.collectionName.help,
-        };
-    },
-    methods: {
-        checkNameStructure() {
-            if (this.$store.state.configuration.ui.collectionName?.checkNameStructure) {
-                let regex = new RegExp(
-                    this.$store.state.configuration.ui.collectionName.checkNameStructure
-                );
-                if (!this.identifier.match(regex)) {
-                    this.error = true;
-                } else {
-                    this.error = false;
-                }
-            }
-        },
-        async createCollectionEntry() {
-            let response = await createCollection({
-                $http: this.$http,
-                identifier: this.identifier,
-            });
-            if (response.status === 200) {
-                this.error = false;
-                this.createNewCollection = false;
-                this.$router.push(`/collections/${this.identifier}`);
-                this.identifier = "";
-            } else {
-                this.identifier = "";
-                this.error = await response.json();
-            }
-        },
-    },
-};
+import { reactive, inject } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+const $store = useStore();
+const $router = useRouter();
+const $http = inject("$http");
+
+const data = reactive({
+    loading: false,
+    identifier: "",
+    form: {},
+    createNewCollection: false,
+    error: false,
+    help: $store.state.configuration.ui.collectionName.help,
+});
+function checkNameStructure() {
+    if ($store.state.configuration.ui.collectionName?.checkNameStructure) {
+        let regex = new RegExp($store.state.configuration.ui.collectionName.checkNameStructure);
+        if (!data.identifier.match(regex)) {
+            data.error = true;
+        } else {
+            data.error = false;
+        }
+    }
+}
+async function createCollectionEntry() {
+    data.loading = true;
+    let response = await createCollection({ $http, identifier: data.identifier });
+    if (response.status === 200) {
+        data.error = false;
+        data.createNewCollection = false;
+        $router.push(`/collections/${data.identifier}`);
+        data.identifier = "";
+    } else {
+        data.identifier = "";
+        data.error = await response.json();
+    }
+    data.loading = false;
+}
 </script>
--- ui/src/components/item/CreateItem.component.vue
+++ ui/src/components/item/CreateItem.component.vue
@@ -1,81 +1,96 @@
 <template>
     <div>
-        <div @click="createNewItem = !createNewItem" class="cursor-pointer">
-	    <i class="fa-solid fa-file-image"></i> Create item
+        <div @click="data.createNewItem = !data.createNewItem" class="cursor-pointer">
+            <i class="fa-solid fa-file-image"></i> Create item
         </div>
-        <el-dialog title="Create a new Item" v-model="createNewItem" width="80%" destroy-on-close>
-            <div class="text-gray-600">
-                <div class="flex flex-col space-y-2">
-                    <div class="flex flex-row">
-                        <div class="text-sm text-gray-600">
-                            Please define an identifier for this item.
-                        </div>
-                        <div class="flex-grow"></div>
-                        <div v-if="error?.code" class="text-red-800">
-                            Error: {{ error.message }}
-                        </div>
+        <el-dialog
+            title="Create a new Item"
+            v-model="data.createNewItem"
+            width="80%"
+            destroy-on-close
+        >
+            <div class="flex flex-col space-y-2 text-gray-600" v-loading="data.loading">
+                <div class="flex flex-row">
+                    <div class="flex-grow"></div>
+                    <div v-if="data.error?.code" class="text-red-800">
+                        Error: {{ data.error.message }}
                     </div>
-                    <div class="w-full">
-                        <el-input
-                            placeholder="item identifier"
-                            v-model="identifier"
-                            @input="checkNameStructure"
-                        ></el-input>
-                    </div>
-                    <div class="text-xs text-gray-600" :class="{ 'text-red-600': error }">
-                        {{ help }}
-                    </div>
-                    <div>
-                        <el-button
-                            type="primary"
-                            @click="createItemEntry"
-                            :disabled="!identifier.length || error"
-                        >
-                            Create this item
-                        </el-button>
-                    </div>
+                </div>
+                <div class="w-full">
+                    <el-form
+                        :model="data.form"
+                        label-width="120px"
+                        @submit.prevent="createItemEntry"
+                    >
+                        <el-form-item label="Identifier" required>
+                            <el-input
+                                :autofocus="true"
+                                placeholder="item identifier"
+                                v-model="data.identifier"
+                                @input="checkNameStructure"
+                            ></el-input>
+                            <div
+                                class="text-xs text-gray-600"
+                                :class="{ 'text-red-600': data.error }"
+                            >
+                                {{ data.help }}
+                            </div>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button
+                                type="primary"
+                                @click="createItemEntry"
+                                :disabled="!data.identifier.length || data.error"
+                            >
+                                Create this item
+                            </el-button>
+                        </el-form-item>
+                    </el-form>
                 </div>
             </div>
         </el-dialog>
     </div>
 </template>
 
-<script>
+<script setup>
 import { createItem } from "./item-services";
-export default {
-    data() {
-        return {
-            identifier: "",
-            createNewItem: false,
-            error: false,
-            help: this.$store.state.configuration.ui.itemName.help,
-        };
-    },
-    methods: {
-        checkNameStructure() {
-            if (this.$store.state.configuration.ui.itemName?.checkNameStructure) {
-                let regex = new RegExp(
-                    this.$store.state.configuration.ui.itemName.checkNameStructure
-                );
-                if (!this.identifier.match(regex)) {
-                    this.error = true;
-                } else {
-                    this.error = false;
-                }
-            }
-        },
-        async createItemEntry() {
-	    let response = await createItem({ $http: this.$http, identifier: this.identifier });
-            if (response.status === 200) {
-                this.error = false;
-                this.createNewItem = false;
-                this.$router.push(`/items/${this.identifier}`);
-                this.identifier = "";
-            } else {
-                this.identifier = "";
-                this.error = await response.json();
-            }
-        },
-    },
-};
+import { reactive, inject } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+const $store = useStore();
+const $router = useRouter();
+const $http = inject("$http");
+
+const data = reactive({
+    loading: false,
+    identifier: "",
+    form: {},
+    createNewItem: false,
+    error: false,
+    help: $store.state.configuration.ui.itemName.help,
+});
+function checkNameStructure() {
+    if ($store.state.configuration.ui.itemName?.checkNameStructure) {
+        let regex = new RegExp($store.state.configuration.ui.itemName.checkNameStructure);
+        if (!data.identifier.match(regex)) {
+            data.error = true;
+        } else {
+            data.error = false;
+        }
+    }
+}
+async function createItemEntry() {
+    data.loading = true;
+    let response = await createItem({ $http, identifier: data.identifier });
+    if (response.status === 200) {
+        data.error = false;
+        data.createNewItem = false;
+        $router.push(`/items/${data.identifier}`);
+        data.identifier = "";
+    } else {
+        data.identifier = "";
+        data.error = await response.json();
+    }
+    data.loading = false;
+}
 </script>

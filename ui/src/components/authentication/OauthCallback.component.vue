a67b8df080a603df5e85dd0d8a30ff9d747e7fc5
Marco La Rosa <m@lr.id.au>
2022-12-02 13:12:07 +1100

    refactor as composition

--- ui/src/components/authentication/OauthCallback.component.vue
+++ ui/src/components/authentication/OauthCallback.component.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-col"></div>
+    <div class="flex flex-col h-40" v-loading="data.loading"></div>
 </template>
 
-<script>
+<script setup>
 import {
     loginSessionKey,
     tokenSessionKey,
@@ -10,36 +10,41 @@ import {
     getLocalStorage,
     removeLocalStorage,
 } from "@/components/storage";
-export default {
-    data() {
-        return {
-            error: false,
-        };
-    },
-    mounted() {
-        this.login();
-    },
-    methods: {
-        async login() {
-            let { code_verifier } = getLocalStorage({ key: loginSessionKey });
-            removeLocalStorage({ key: loginSessionKey });
-            let response = await this.$http.post({
-                route: `/auth/${this.$route.query.state}/code`,
-                body: { code: this.$route.query.code, code_verifier },
-            });
-            if (response.status !== 200) {
-                this.error = true;
-                await new Promise((resolve) => setTimeout(resolve, 2000));
-                await this.$router.push("/login");
-            } else {
-                let { token } = await response.json();
-                let user = JSON.parse(atob(token.split(".")[1]));
-                this.$store.commit("setUserData", user);
 
-                putLocalStorage({ key: tokenSessionKey, data: { token } });
-                await this.$router.push("/");
-            }
-        },
-    },
-};
+import { reactive, onMounted, inject } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useStore } from "vuex";
+const $route = useRoute();
+const $router = useRouter();
+const $store = useStore();
+const $http = inject("$http");
+
+const data = reactive({
+    error: false,
+    loading: true,
+});
+onMounted(() => {
+    login();
+});
+async function login() {
+    const { code } = $route.query;
+    let { code_verifier } = getLocalStorage({ key: loginSessionKey });
+    removeLocalStorage({ key: loginSessionKey });
+    let response = await $http.post({
+        route: `/auth/${$route.query.state}/code`,
+        body: { code, code_verifier },
+    });
+    if (response.status !== 200) {
+        data.error = true;
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await $router.push("/login");
+    } else {
+        let { token } = await response.json();
+        let user = JSON.parse(atob(token.split(".")[1]));
+        $store.commit("setUserData", user);
+
+        putLocalStorage({ key: tokenSessionKey, data: { token } });
+        await $router.push("/");
+    }
+}
 </script>

b869889112dd6c406eb12869b196c32d8e9febf3
Marco La Rosa <m@lr.id.au>
2022-12-12 14:35:04 +1100

    revise to support bulk user attach - closes #129

--- ui/src/components/collection/ViewCollection/Administration/InviteUser.component.vue
+++ ui/src/components/collection/ViewCollection/Administration/InviteUser.component.vue
@@ -1,62 +1,72 @@
 <template>
     <div class="flex flex-col">
-        <view-collection-users-component :users="users" @refresh="getCollectionUsers" />
+        <view-collection-users-component :users="data.users" @refresh="loadCollectionUsers" />
 
         <div class="mt-6">
             <div class="text-gray-600">Invite users to work on this collection with you</div>
             <div class="flex flex-col my-2">
                 <div class="flex flex-row">
                     <div class="flex-grow">
-                        <el-input v-model="email" placeholder="Search by user email address" />
+                        <el-input
+                            type="textarea"
+                            rows="5"
+                            v-model="data.emails"
+                            placeholder="Search by user email address"
+                        />
                     </div>
                     <div>
-                        <el-button @click="attachUser">attach user</el-button>
+                        <el-button @click="attachUsers">attach user</el-button>
                     </div>
                 </div>
                 <div class="text-xs text-gray-600">
                     Invite users by their email address. You will need to provide their email
-                    exactly as the system knows it in order to find them.
+                    exactly as the system knows it in order to find them. Specify multiple users by
+                    comma or one per line.
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
 import { attachUser, getCollectionUsers } from "@/components/collection/collection-services";
 import ViewCollectionUsersComponent from "./ViewCollectionUsers.component.vue";
+import { flattenDeep } from "lodash";
+import { reactive, onMounted, inject } from "vue";
+import { useRoute } from "vue-router";
+const $route = useRoute();
+const $http = inject("$http");
 
-export default {
-    components: {
-        ViewCollectionUsersComponent,
-    },
-    data() {
-        return { identifier: this.$route.params.identifier, email: undefined, users: [] };
-    },
-    mounted() {
-        this.getCollectionUsers();
-    },
-    methods: {
-        async attachUser() {
-            let response = await attachUser({
-                $http: this.$http,
-                identifier: this.identifier,
-                email: this.email,
+const data = reactive({
+    emails: undefined,
+    users: [],
+});
+onMounted(() => {
+    loadCollectionUsers();
+});
+async function attachUsers() {
+    let emails = data.emails.split("\n").map((line) => line.split(",").map((e) => e.trim()));
+    emails = flattenDeep(emails);
+    await Promise.all(
+        emails.map((email) => {
+            return attachUser({
+                $http,
+                identifier: $route.params.identifier,
+                email,
             });
-            if (response.status === 200) {
-                this.getCollectionUsers();
-            }
-            this.email = undefined;
-        },
-        async getCollectionUsers() {
-            let response = await getCollectionUsers({
-                $http: this.$http,
-                identifier: this.identifier,
-            });
-            if (response.status === 200) {
-                this.users = (await response.json()).users;
-            }
-        },
-    },
-};
+        })
+        // data.emails = undefined;
+    );
+    loadCollectionUsers();
+    data.emails = undefined;
+}
+async function loadCollectionUsers() {
+    let response = await getCollectionUsers({
+        $http,
+        identifier: $route.params.identifier,
+    });
+    if (response.status === 200) {
+        data.users = (await response.json()).users;
+    }
+}
 </script>

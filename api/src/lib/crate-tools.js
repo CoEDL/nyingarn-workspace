de124dde7ffa030dd4d5422eb4e9e8db482c6699
Marco La Rosa <m@lr.id.au>
2022-12-16 10:57:19 +1100

    refactor registerAllFiles to use ro crate lib
    Move crate related methods from describo route handler to here.

--- api/src/lib/crate-tools.js
+++ api/src/lib/crate-tools.js
@@ -2,12 +2,10 @@ import { getStoreHandle } from "../common/index.js";
 import path from "path";
 import mime from "mime-types";
 import lodashPkg from "lodash";
-const { difference } = lodashPkg;
+const { difference, isArray } = lodashPkg;
 
 // TODO: this code does not have tests
-export async function registerAllFiles({ id, className, crate }) {
-    let store = await getStoreHandle({ id, className });
-
+export async function registerAllFiles({ store, crate }) {
     // get list of files in the bucket
     let files = (await store.listResources()).filter((file) => {
         return ![
@@ -17,7 +15,7 @@ export async function registerAllFiles({ id, className, crate }) {
         ].includes(file.Key);
     });
     files = files.filter((file) => !file.Key.match(/^\./));
-    if (!files.length) return { crate, filesAdded: [] };
+    if (!files.length) return { crate };
 
     // determine which files not already registered in the crate
     const filesInBucket = files.map((file) => file.Key);
@@ -39,27 +37,23 @@ export async function registerAllFiles({ id, className, crate }) {
                 hasPart: [{ "@id": "./" }],
             },
         };
-        crate["@graph"].push(entity);
+        crate.addEntity(entity);
+        let parts = crate.rootDataset.hasPart;
+        parts.push({ "@id": entity["@id"] });
+        crate.rootDataset.hasPart = parts;
     }
 
-    // link the missing files into the hasPart prop of the rootDataset
-    let rootDescriptor = crate["@graph"].filter(
-        (e) => e["@id"] === "ro-crate-metadata.json" && e["@type"] === "CreativeWork"
-    )[0];
-    crate["@graph"] = crate["@graph"].map((e) => {
-        if (e["@id"] === rootDescriptor.about["@id"]) {
-            e.hasPart = [...e.hasPart, ...filesToAdd.map((file) => ({ "@id": file }))];
-        }
-        return e;
-    });
-
     // set the mimetype on all files in the crate
-    crate["@graph"] = crate["@graph"].map((entity) => {
-        return { encodingFormat: getMimeType(entity["@id"]), ...entity };
-    });
+    let entities = crate.entities();
+    for (let entity of entities) {
+        if (crate.hasType(entity, "File")) {
+            entity = { encodingFormat: getMimeType(entity["@id"]), ...entity };
+            crate.addEntity(entity, true);
+        }
+    }
 
     // upload the new crate file
-    return { crate, filesAdded: filesToAdd };
+    return crate;
 
     function getMimeType(filename) {
         let mimetype = mime.lookup(filename);
@@ -74,4 +68,40 @@ export async function registerAllFiles({ id, className, crate }) {
         }
         return mimetype;
     }
+}
+
+export function getContext() {
+    return [
+        "https://w3id.org/ro/crate/1.1/context",
+        "http://purl.archive.org/language-data-commons/context.json",
+        {
+            "@vocab": "http://schema.org/",
+        },
+        {
+            "@base": null,
+        },
+    ];
+}
+
+export function createDefaultROCrateFile({ name }) {
+    return {
+        "@context": getContext(),
+        "@graph": [
+            {
+                "@id": "ro-crate-metadata.json",
+                "@type": "CreativeWork",
+                conformsTo: {
+                    "@id": "https://w3id.org/ro/crate/1.1/context",
+                },
+                about: {
+                    "@id": "./",
+                },
+            },
+            {
+                "@id": "./",
+                "@type": "Dataset",
+                name: name,
+            },
+        ],
+    };
 }

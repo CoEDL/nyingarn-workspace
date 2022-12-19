d4d3868f64ae135f4056d8e6e958674d494a1b20
Marco La Rosa <m@lr.id.au>
2022-12-19 11:52:43 +1100

    bugfix middleware failure conditions

--- api/src/common/middleware.js
+++ api/src/common/middleware.js
@@ -20,6 +20,13 @@ export async function demandAuthenticatedUser(req, res) {
             token: req.headers.authorization.split("Bearer ")[1],
             configuration: req.session.configuration,
         });
+<<<<<<< Updated upstream
+=======
+        user = await getUser({ userId: user.id });
+        if (!user) {
+            return res.unauthorized();
+        }
+>>>>>>> Stashed changes
         req.session.user = user;
     } catch (error) {
         return res.unauthorized();
@@ -32,17 +39,54 @@ export async function demandAdministrator(req, res) {
     }
 }
 
+<<<<<<< Updated upstream
 export async function requireIdentifierAccess(req, res) {
+=======
+export async function requireCollectionAccess(req, res) {
+    if (!req.params.identifier) {
+        return res.badRequest(`Identifier not defined`);
+    }
+
+    try {
+        let collection = await lookupCollectionByIdentifier({
+            identifier: req.params.identifier,
+            userId: req.session.user.id,
+        });
+        if (!collection) {
+            return res.forbidden(`You don't have permission to access this endpoint`);
+        }
+    } catch (error) {
+        return res.forbidden(`You don't have permission to access this endpoint`);
+    }
+    req.session.collection = collection;
+}
+
+export async function requireItemAccess(req, res) {
+>>>>>>> Stashed changes
     if (!req.body?.identifier && !req.params?.identifier) {
         return res.badRequest(`No identifier defined in body or params`);
     }
     const identifier = req.body?.identifier ? req.body?.identifier : req.params?.identifier;
+<<<<<<< Updated upstream
     let item = await lookupItemByIdentifier({
         userId: req.session.user.id,
         identifier: identifier,
     });
     if (!item) {
         return res.forbidden(`You don't have access to that item`);
+=======
+    let item;
+    try {
+        item = await lookupItemByIdentifier({
+            identifier,
+            userId: req.session.user.id,
+        });
+        if (!item) {
+            return res.forbidden(`You don't have permission to access this endpoint`);
+        }
+    } catch (error) {
+        return res.forbidden(`You don't have permission to access this endpoint`);
+>>>>>>> Stashed changes
     }
     req.session.item = item;
 }

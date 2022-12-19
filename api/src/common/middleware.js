6fb990b08443e0ed3fbad1d03854e5c17a72c1c0
Marco La Rosa <m@lr.id.au>
2022-12-19 11:55:09 +1100

    fix middleware

--- api/src/common/middleware.js
+++ api/src/common/middleware.js
@@ -3,14 +3,6 @@ import { verifyToken } from "./jwt.js";
 import { lookupItemByIdentifier } from "../lib/item.js";
 const log = getLogger();
 
-export function route(handler) {
-    return [demandAuthenticatedUser, handler];
-}
-
-export function routeAdmin(handler) {
-    return [demandAuthenticatedUser, demandAdministrator, handler];
-}
-
 export async function demandAuthenticatedUser(req, res) {
     if (!req.headers.authorization) {
         return res.unauthorized();
@@ -20,13 +12,10 @@ export async function demandAuthenticatedUser(req, res) {
             token: req.headers.authorization.split("Bearer ")[1],
             configuration: req.session.configuration,
         });
-<<<<<<< Updated upstream
-=======
         user = await getUser({ userId: user.id });
         if (!user) {
             return res.unauthorized();
         }
->>>>>>> Stashed changes
         req.session.user = user;
     } catch (error) {
         return res.unauthorized();
@@ -39,9 +28,6 @@ export async function demandAdministrator(req, res) {
     }
 }
 
-<<<<<<< Updated upstream
-export async function requireIdentifierAccess(req, res) {
-=======
 export async function requireCollectionAccess(req, res) {
     if (!req.params.identifier) {
         return res.badRequest(`Identifier not defined`);
@@ -62,19 +48,10 @@ export async function requireCollectionAccess(req, res) {
 }
 
 export async function requireItemAccess(req, res) {
->>>>>>> Stashed changes
     if (!req.body?.identifier && !req.params?.identifier) {
         return res.badRequest(`No identifier defined in body or params`);
     }
     const identifier = req.body?.identifier ? req.body?.identifier : req.params?.identifier;
-<<<<<<< Updated upstream
-    let item = await lookupItemByIdentifier({
-        userId: req.session.user.id,
-        identifier: identifier,
-    });
-    if (!item) {
-        return res.forbidden(`You don't have access to that item`);
-=======
     let item;
     try {
         item = await lookupItemByIdentifier({
@@ -86,7 +63,6 @@ export async function requireItemAccess(req, res) {
         }
     } catch (error) {
         return res.forbidden(`You don't have permission to access this endpoint`);
->>>>>>> Stashed changes
     }
     req.session.item = item;
 }

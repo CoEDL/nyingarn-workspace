da0255c98ddd3402704db9dee4fa8df033309106
Marco La Rosa <m@lr.id.au>
2023-03-20 14:10:39 +1100

    move buttons before the text

--- ui/src/components/item/ViewItem/Administration/PermissionFilesDisplay.component.vue
+++ ui/src/components/item/ViewItem/Administration/PermissionFilesDisplay.component.vue
@@ -2,14 +2,6 @@
     <div class="flex flex-col space-y-2 bg-blue-100 p-4">
         <div class="border-l-2 pl-2 flex flex-row space-x-4">
             <div class="flex flex-row space-x-2" v-if="data.rightsHolderPermissionLoaded">
-                <a
-                    class="underline"
-                    :href="data.rightsHolderPermissionLoaded"
-                    v-if="data.rightsHolderPermissionLoaded"
-                >
-                    <i class="fa-regular fa-file-pdf"></i>&nbsp;Download the Rights Holder
-                    Permission
-                </a>
                 <div>
                     <el-button
                         @click="deletePermissionForm('rightsHolderPermission')"
@@ -18,15 +10,19 @@
                         <i class="fa-solid fa-trash"></i>
                     </el-button>
                 </div>
+                <a
+                    class="underline"
+                    :href="data.rightsHolderPermissionLoaded"
+                    v-if="data.rightsHolderPermissionLoaded"
+                >
+                    <i class="fa-regular fa-file-pdf"></i>&nbsp;Download the Rights Holder
+                    Permission
+                </a>
             </div>
             <div v-else>Rights Holder Permission has not been loaded</div>
         </div>
         <div class="border-l-2 pl-2 flex flex-row space-x-4">
             <div class="flex flex-row space-x-2" v-if="data.languageAuthorityPermissionLoaded">
-                <a class="underline" :href="data.languageAuthorityPermissionLoaded">
-                    <i class="fa-regular fa-file-pdf"></i>&nbsp;Download the Language Authority
-                    Permission
-                </a>
                 <div>
                     <el-button
                         @click="deletePermissionForm('languageAuthorityPermission')"
@@ -35,6 +31,10 @@
                         <i class="fa-solid fa-trash"></i>
                     </el-button>
                 </div>
+                <a class="underline" :href="data.languageAuthorityPermissionLoaded">
+                    <i class="fa-regular fa-file-pdf"></i>&nbsp;Download the Language Authority
+                    Permission
+                </a>
             </div>
             <div v-else>Language Authority Permission has not been loaded</div>
         </div>

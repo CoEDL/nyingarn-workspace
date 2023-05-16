c12af857f151d7d8c31c1f1bc83f60ea8bf097d8
Marco La Rosa <m@lr.id.au>
2023-05-16 12:21:47 +1000

    refactor; wip controls to trigger an item to be indexed

---
+++ ui/src/components/admin/IndexItem.component.vue
@@ -0,0 +1,67 @@
+<template>
+    <el-card>
+        <template #header>Index a specific item or collection in the repository</template>
+        <div class="flex flex-row space-x-1">
+            <el-select
+                class="w-full"
+                v-model="data.selectedItemIdentifier"
+                filterable
+                remote
+                reserve-keyword
+                placeholder="Search for an item to index"
+                :remote-method="findItem"
+                :loading="data.loading"
+            >
+                <el-option
+                    v-for="item in data.options"
+                    :key="item.identifier"
+                    :label="item.identifier"
+                    :value="item.id"
+                >
+                    {{ item.identifier }} ({{ item.type }})
+                </el-option>
+            </el-select>
+            <div><el-button @click="indexItem" type="primary">Index item</el-button></div>
+        </div>
+    </el-card>
+</template>
+
+<script setup>
+import { ElCard, ElSelect, ElOption } from "element-plus";
+import * as lib from "./lib.js";
+import { reactive, inject, onMounted } from "vue";
+const $http = inject("$http");
+
+const data = reactive({
+    selectedItem: undefined,
+    options: [],
+    items: {
+        prefix: undefined,
+        total: 0,
+        currentPage: 1,
+        rows: [],
+    },
+    collections: {
+        prefix: undefined,
+        total: 0,
+        currentPage: 1,
+        rows: [],
+    },
+});
+
+onMounted(() => {
+    findItem();
+});
+
+async function findItem(prefix) {
+    data.loading = true;
+    let { rows, total } = await lib.lookupRepositoryContent({ $http, offset: 0, prefix });
+    data.options = [...rows];
+    data.loading = false;
+    return rows;
+}
+async function indexItem(item) {
+    await lib.indexRepositoryContent({ $http, id: data.selectedItemIdentifier });
+    data.selectedItemIdentifier = undefined;
+}
+</script>
--- ui/src/components/admin/lib.js
+++ ui/src/components/admin/lib.js
@@ -1,3 +1,50 @@
+export async function loadItems({ $http, offset, prefix }) {
+    const params = { offset };
+    if (prefix) params.prefix = prefix;
+    let response = await $http.get({
+        route: "/admin/entries/items",
+        params,
+    });
+    if (response.status === 200) {
+        let { items, total } = await response.json();
+        return { rows: items, total };
+    } else {
+        return { items: [], total: 0 };
+    }
+}
+export async function loadCollections({ $http, offset, prefix }) {
+    const params = { offset };
+    if (prefix) params.prefix = prefix;
+    let response = await $http.get({
+        route: "/admin/entries/collections",
+        params,
+    });
+    if (response.status === 200) {
+        let { collections, total } = await response.json();
+        return { rows: collections, total };
+    } else {
+        return { collections: [], total: 0 };
+    }
+}
+export async function lookupRepositoryContent({ $http, offset, prefix }) {
+    const params = { offset };
+    if (prefix) params.prefix = prefix;
+    let response = await $http.get({
+        route: "/repository/lookup-content",
+        params,
+    });
+    if (response.status === 200) {
+        let { items, total } = await response.json();
+        return { rows: items, total };
+    } else {
+        return { items: [], total: 0 };
+    }
+}
+export async function indexRepositoryContent({ $http, id }) {
+    let response = await $http.get({
+        route: `/repository/index/${id}`,
+    });
+}
 export async function connectItem({ $http, identifier }) {
     await $http.put({ route: `/admin/items/${identifier}/connect-user` });
 }

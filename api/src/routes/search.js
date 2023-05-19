e832e726d9e23c6737479051c9f0a3157417b5fa
Marco La Rosa <m@lr.id.au>
2023-05-19 14:03:40 +1000

    revise search route to also include index to use

--- api/src/routes/search.js
+++ api/src/routes/search.js
@@ -1,17 +1,18 @@
-import { demandAuthenticatedUser, loadConfiguration } from "../common/index.js";
+import { demandAuthenticatedUser } from "../common/index.js";
 import fetch from "cross-fetch";
 
 export function setupRoutes(fastify, options, done) {
     fastify.addHook("preHandler", demandAuthenticatedUser);
 
-    fastify.post("/search/datapacks", postSearchDataPacksHandler);
+    fastify.post("/search/:index", postSearchDataPacksHandler);
     done();
 }
 
 // TODO: this code does not have tests
 async function postSearchDataPacksHandler(req) {
     const configuration = req.session.configuration;
-    let response = await fetch(`${configuration.api.services.elastic.host}/_search`, {
+    const { index } = req.params;
+    let response = await fetch(`${configuration.api.services.elastic.host}/${index}/_search`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",

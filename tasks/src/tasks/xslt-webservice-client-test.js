f450b824afd8eb20d8f0cb5d9dfbac88939ba938
Conal Tuohy <conal.tuohy@gmail.com>
2023-07-17 22:45:02 +1000

    serialization of surface files

--- tasks/src/tasks/xslt-webservice-client-test.js
+++ tasks/src/tasks/xslt-webservice-client-test.js
@@ -1,11 +1,16 @@
 //import fetch, { FormData, File, fileFrom } from 'node-fetch'
 import {fileFromPath} from 'formdata-node/file-from-path';
 import axios from 'axios';
-import { DOMParser } from 'xmldom';
+import { DOMParser, XMLSerializer } from 'xmldom';
 import fsExtra from 'fs-extra';
 const { writeFile } = fsExtra;
 
 test();
+
+// NB xmldom's 'children' and 'firstElementChild' properties don't work
+function childElements(element) {
+	return Array.from(element.childNodes).filter(node => node.nodeType === 1); // nodeType 1 is an element
+}
 
 export async function test() {
 	try {
@@ -17,31 +22,33 @@ export async function test() {
 		form.append("source", source);
 		const {data} = await axios.postForm('http://xml:8080/nyingarn/ingest-tei', form);
 		const surfacesDocument = new DOMParser().parseFromString(data, 'application/xml');
+		const serializer = new XMLSerializer();
 		const directoryElement = surfacesDocument.documentElement;
-		const childNodes = directoryElement.childNodes;
-		console.log(childNodes.length);
-		console.log(childNodes[0]);
-		for (var i = 0; i < childNodes.length; i++) {
-			var childNode = childNodes[i];
-			if (childNode.nodeType === 1) {
+		// NB xmldom's 'children' and 'firstElementChild' properties don't work
+		const childNodes = childElements(directoryElement);
+		childNodes.forEach(
+			function(childNode) {
 				// childNode is a c:file element
 				var fileName = childNode.getAttribute("name");
-				
-				console.log(fileName);
+				// NB xmldom's 'children' and 'firstElementChild' properties don't work
+				var surfaceElement = childElements(childNode)[0];
 				writeFile(
 				  `/tmp/${fileName}`, 
-				  serializer.serializeToString(childNode), 
+				  serializer.serializeToString(surfaceElement), 
 				  function(error) {
 					if (error) {
-					  console.log(error);
+					  console.log(`Surface file ${fileName} could not be saved`);
 					} else {
 					  console.log(`Surface file ${fileName} saved`);
 					}
 				  }
 				); 				
 			}
-		}
+		);
 	} catch (error) {
-		console.log(error);
+		// the Axios error has a "response" property which is an object representing the HTTP response,
+		// which in turn as a "data" property which is our XSLT error as a JS object deserialized from the 
+		// JSON body of the response. This "data" object should just be thrown as an error
+		console.log(error.response.data);
 	}
 }

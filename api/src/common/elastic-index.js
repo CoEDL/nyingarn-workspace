0672c201b1c3122d8b744c165cef158c34f69010
Marco La Rosa <m@lr.id.au>
2023-08-25 09:58:09 +1000

    refactor indexing code
    * revise how the document gets indexed; produce a summary doc with what
      we need rather than just slurping up the crate file
    * revise which entities get indexed; some rather than all blindly which
      serves no purpose.

--- api/src/common/elastic-index.js
+++ api/src/common/elastic-index.js
@@ -1,20 +1,25 @@
 import { loadConfiguration } from "./index.js";
 import lodashPkg from "lodash";
-const { isArray, isString, isPlainObject } = lodashPkg;
+const { isArray, isString, isPlainObject, flattenDeep } = lodashPkg;
 import { Client } from "@elastic/elasticsearch";
 import { ROCrate } from "ro-crate";
+const typesToExcludeFromIndex = ["File", "GeoShape", "GeoCoordinates", "Language"];
 
-export async function indexItem({ item, crate }) {
-    let configuration = await loadConfiguration();
+export async function indexItem({ configuration, item, crate }) {
+    // let configuration = await loadConfiguration();
+    // console.log(configuration);
+    // console.log(item);
+    // console.log(crate);
     crate = new ROCrate(crate, { array: true, link: true });
-    let document = crate.getTree({ valueObject: false });
+    // let document = crate.getTree({ valueObject: false });
 
     const client = new Client({
         node: configuration.api.services.elastic.host,
     });
 
-    // ensure we have all of the indices we need
+    // setup the metadata index
     try {
+        // await client.indices.delete({ index: "metadata" });
         await client.indices.get({ index: "metadata" });
     } catch (error) {
         await client.indices.create({
@@ -22,40 +27,31 @@ export async function indexItem({ item, crate }) {
             settings: { "index.mapping.ignore_malformed": true },
         });
     }
-    try {
-        await client.indices.get({ index: "entities" });
-    } catch (error) {
-        await client.indices.create({ index: "entities" });
-    }
 
     const indexIdentifier = `/${item.type}/${item.identifier}`;
+    const document = assembleIndexRecord({ crate });
+    // console.log(document);
     await client.index({
         index: "metadata",
         id: indexIdentifier,
         document,
     });
 
-    for (let entity of crate.entities()) {
-        if (!["ro-crate-metadata.json", "./"].includes(entity["@id"])) {
-            if (!isArray(entity["@type"])) entity["@type"] = [entity["@type"]];
-
-            entity = entity.toJSON();
-            if (!isArray(entity["@type"])) entity["@type"] = [entity["@type"]];
-            for (let type of entity["@type"]) {
-                //  walk the entity and remove links to other things
-                for (let property of Object.keys(entity)) {
-                    if (!isArray(entity[property]) && !isPlainObject(entity[property])) continue;
-                    if (isPlainObject(entity[property])) entity[property] = [entity[property]];
-                    entity[property] = entity[property].filter((instance) => isString(instance));
-                    if (!entity[property].length) delete entity[property];
-                }
-                await client.index({
-                    index: "entities",
-                    id: encodeURIComponent(entity["@id"]),
-                    document: entity,
-                });
-            }
-        }
+    // setup the entities index
+    try {
+        // await client.indices.delete({ index: "entities" });
+        await client.indices.get({ index: "entities" });
+    } catch (error) {
+        await client.indices.create({ index: "entities" });
+    }
+    let entities = assembleEntityLookupRecords({ crate });
+    // console.log(entities);
+    for (let entity of entities) {
+        await client.index({
+            index: "entities",
+            id: encodeURIComponent(entity["@id"]),
+            document: entity,
+        });
     }
 }
 
@@ -69,4 +65,122 @@ export async function deleteItemFromIndex({ item, configuration }) {
         index: "metadata",
         id: indexIdentifier,
     });
+}
+
+// TODO this method does not have tests
+export function assembleIndexRecord({ crate }) {
+    // console.log(crate);
+
+    let geography = extractGeography({ crate });
+    let record = {
+        name: crate.rootDataset?.name?.join(" ") ?? "",
+        description: crate.rootDataset?.description?.join(" ") ?? "",
+        identifier: crate.rootDataset?.identifier?.[0] ?? "",
+        subjectLanguage:
+            flattenDeep(
+                crate.rootDataset?.subjectLanguage?.map((l) => {
+                    return [l.name, l.alternateName, l.languageCode];
+                })
+            ) ?? [],
+        contentLanguage:
+            flattenDeep(
+                crate.rootDataset?.contentLanguage?.map((l) => {
+                    return [l.name, l.alternateName, l.languageCode];
+                })
+            ) ?? [],
+        access: crate.rootDataset?.licence?.[0].name?.[0] ?? "Restricted",
+        location: geography,
+    };
+    // console.log(record);
+    return record;
+}
+
+// TODO this method does not have tests
+export function extractGeography({ crate }) {
+    let entities = [];
+    for (let entity of crate.entities()) {
+        if (entity["@type"].includes("GeoShape") || entity["@type"].includes("GeoCoordinates")) {
+            entities.push(entity);
+        }
+    }
+
+    entities.push({
+        "@id": "test",
+        "@type": ["GeoShape"],
+        name: "aaa",
+        geojson: [
+            '{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[58.07810783386231,29.095852076791065],[58.07810783386231,-19.12303093039881],[11.671857833862305,-19.12303093039881],[11.671857833862305,29.095852076791065]]]}}',
+        ],
+    });
+    const geography = entities.map((entity) => {
+        if (entity?.geojson?.length) {
+            let geojson = JSON.parse(entity.geojson[0]);
+
+            let coordinates = geojson?.geometry?.coordinates.map((e) => {
+                if (isString(e)) return parseFloat(e);
+                if (isArray(e)) {
+                    return [
+                        e.map((c) => {
+                            return parseFloat(c);
+                        }),
+                    ];
+                }
+            });
+            // console.log(JSON.stringify(coordinates, null, 2));
+            return coordinates;
+        }
+    });
+
+    // console.log(JSON.stringify(geography, null, 2));
+
+    return geography;
+
+    // const feature = {
+    //     type: "Feature",
+    //     properties: { name: "Yorta Yorta" },
+    //     geometry: { type: "Point", coordinates: [145.26357989848, -36.093929709321] },
+    // };
+
+    // const feature = {
+    //     type: "Feature",
+    //     geometry: {
+    //         type: "Polygon",
+    //         coordinates: [
+    //             [
+    //                 [144.56249892711642, -27.126287638023676],
+    //                 [144.56249892711642, -29.486150057382325],
+    //                 [138.27831923961642, -29.486150057382325],
+    //                 [138.27831923961642, -27.126287638023676],
+    //             ],
+    //         ],
+    //     },
+    // };
+}
+
+// TODO this method does not have tests
+export function assembleEntityLookupRecords({ crate }) {
+    let entities = [];
+    for (let entity of crate.entities()) {
+        if (["ro-crate-metadata.json", "./"].includes(entity["@id"])) continue;
+
+        entity = entity.toJSON();
+        if (!isArray(entity["@type"])) entity["@type"] = [entity["@type"]];
+
+        // we don't index all types in the crate
+        let skipEntity = false;
+        for (let type of typesToExcludeFromIndex) {
+            if (entity["@type"].includes(type)) skipEntity = true;
+        }
+        if (skipEntity) continue;
+
+        //  walk the entity and remove links to other things
+        for (let property of Object.keys(entity)) {
+            if (!isArray(entity[property]) && !isPlainObject(entity[property])) continue;
+            if (isPlainObject(entity[property])) entity[property] = [entity[property]];
+            entity[property] = entity[property].filter((instance) => isString(instance));
+            if (!entity[property].length) delete entity[property];
+        }
+        entities.push(entity);
+    }
+    return entities;
 }

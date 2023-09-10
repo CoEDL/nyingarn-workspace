c5091aa6cb54ae7ff6c57c806ba2b684faa8ccfb
Marco La Rosa <m@lr.id.au>
2023-09-11 09:40:23 +1000

    refactor metadata component

---
+++ ui/src/components/item/ViewItem/Metadata.component.vue
@@ -0,0 +1,102 @@
+<template>
+    <describo-crate-builder
+        :style="{ height: metadataPanelHeight }"
+        class="overflow-scroll"
+        :crate="data.crate"
+        :profile="data.profile"
+        :lookup="lookup"
+        :entity-id="data.entityId"
+        :enable-context-editor="false"
+        :enable-crate-preview="true"
+        :enable-browse-entities="true"
+        :enable-internal-routing="false"
+        :show-controls="true"
+        :purge-unlinked-entities-before-save="true"
+        tab-location="right"
+        @save:crate="saveCrate"
+        @navigation="updateRoute"
+    >
+    </describo-crate-builder>
+</template>
+
+<script setup>
+import { reactive, onMounted, onBeforeUnmount, inject, watch, computed } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { ElMessage } from "element-plus";
+import { Lookup } from "../../lookup.js";
+import isEmpty from "lodash-es/isEmpty.js";
+const $route = useRoute();
+const $router = useRouter();
+const $http = inject("$http");
+const lookup = new Lookup({ $http });
+
+let props = defineProps({
+    identifier: {
+        type: String,
+    },
+});
+
+let data = reactive({
+    routeWatcher: undefined,
+    entityId: undefined,
+    crate: undefined,
+    profile: undefined,
+});
+onMounted(() => {
+    load();
+    data.routeWatcher = watch(
+        () => $route.query?.id,
+        (n, o) => {
+            if (n !== o && n !== undefined) data.entityId = decodeURIComponent(atob(n));
+        }
+    );
+});
+onBeforeUnmount(() => {
+    data.routeWatcher();
+});
+const metadataPanelHeight = computed(() => {
+    return `${window.innerHeight - 140}px`;
+});
+
+function updateRoute(entity) {
+    const id = btoa(encodeURIComponent(entity["@id"]));
+    if (isEmpty($route?.query)) {
+        $router?.replace({ query: { id } });
+    } else {
+        $router?.push({ query: { id } });
+    }
+    data.entityId = id;
+}
+async function load() {
+    if (!$route.meta.type && !$route.params.identifier) return;
+
+    // load the crate file
+    let response = await $http.get({
+        route: `/describo/rocrate/${$route.meta.type}/${$route.params.identifier}`,
+    });
+    if (response.status !== 200) {
+        ElMessage.error(`Unable to retrieve RO Crate file`);
+    }
+    data.crate = (await response.json()).crate;
+
+    // load the profile
+    response = await $http.get({
+        route: `/describo/profile/${$route.meta.type}`,
+    });
+    if (response.status !== 200) {
+        ElMessage.error(`Unable to retrieve profile`);
+    }
+    response = await response.json();
+    data.profile = response.profile;
+}
+async function saveCrate(data) {
+    let response = await $http.put({
+        route: `/describo/rocrate/${$route.meta.type}/${$route.params.identifier}`,
+        body: { data },
+    });
+    if (response.status !== 200) {
+        ElMessage.error(`Unable to save RO Crate file`);
+    }
+    // console.log("SAVE CRATE", JSON.stringify(data, null, 2));
+}
+</script>
--- ui/src/components/item/ViewItem/Shell.component.vue
+++ ui/src/components/item/ViewItem/Shell.component.vue
@@ -7,22 +7,7 @@
                     <view-item-content-component v-if="data.activeTab === 'view'" />
                 </el-tab-pane>
                 <el-tab-pane label="Item Metadata" name="metadata">
-                    <describo-crate-builder
-                        v-if="data.activeTab === 'metadata' && data.metadataComponent.display"
-                        :style="{ height: metadataPanelHeight }"
-                        class="overflow-scroll"
-                        :crate="data.crate"
-                        :profile="data.profile"
-                        :lookup="lookup"
-                        :enable-context-editor="false"
-                        :enable-crate-preview="true"
-                        :enable-browse-entities="true"
-                        :enable-internal-routing="true"
-                        :purge-unlinked-entities-before-save="true"
-                        @ready="data.metadataComponent.loading = false"
-                        @save:crate="saveCrate"
-                    >
-                    </describo-crate-builder>
+                    <MetadataComponent v-if="data.activeTab === 'metadata'" />
                 </el-tab-pane>
                 <el-tab-pane label="Upload Data" name="upload">
                     <upload-component v-if="data.activeTab === 'upload'" />
@@ -45,17 +30,13 @@ import UploadComponent from "./UploadComponent/Shell.component.vue";
 import AdministrationComponent from "./Administration/Shell.component.vue";
 import PublishComponent from "./PublishComponent/Shell.component.vue";
 import NavbarComponent from "./Navbar.component.vue";
+import MetadataComponent from "./Metadata.component.vue";
 import { reactive, onMounted, onBeforeMount, inject, watch, computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
-import { useStore } from "vuex";
 import { ElMessage } from "element-plus";
-import { Lookup } from "../../lookup.js";
-import { debounce } from "lodash";
-const $store = useStore();
 const $route = useRoute();
 const $router = useRouter();
 const $http = inject("$http");
-const lookup = new Lookup({ $http });
 
 let props = defineProps({
     identifier: {
@@ -68,14 +49,6 @@ let data = reactive({
     routeWatcher: undefined,
     tabs: ["view", "metadata", "associate", "upload", "administration", "publish"],
     activeTab: "view",
-    metadataComponent: {
-        display: false,
-        loading: false,
-    },
-    crate: {},
-    profile: {},
-    debouncedLoad: debounce(load, 300),
-    isAdmin: $store.state.user.administrator,
 });
 onBeforeMount(async () => {
     await checkUserAccess();
@@ -83,7 +56,6 @@ onBeforeMount(async () => {
 onMounted(() => {
     data.routeWatcher = watch(() => $route.path, updateRouteOnNav);
     updateRouteOnNav();
-    if (data.activeTab === "metadata") loadMetadata();
 });
 const metadataPanelHeight = computed(() => {
     return `${window.innerHeight - 140}px`;
@@ -98,8 +70,6 @@ async function checkUserAccess() {
     data.userIsPermitted = true;
 }
 function updateRouteOnNav() {
-    data.metadataComponent.display = false;
-    if (data.activeTab === "metadata") loadMetadata();
     if (!$route.name.match(/^items/)) {
         data.routeWatcher();
         return;
@@ -112,50 +82,6 @@ function updateRouteOnNav() {
     }
 }
 function updateRouteOnTabSelect(tab) {
-    data.metadataComponent.display = false;
     $router.push(tab.paneName);
-    if (tab.paneName === "metadata") loadMetadata();
-}
-async function loadMetadata() {
-    await load();
-    await new Promise((resolve) => setTimeout(resolve, 50));
-    data.metadataComponent.display = true;
-}
-async function load() {
-    if (!$route.meta.type && !$route.params.identifier) return;
-    data.metadataComponent.loading = true;
-
-    // load the crate file
-    let response = await $http.get({
-        route: `/describo/rocrate/${$route.meta.type}/${$route.params.identifier}`,
-    });
-    if (response.status !== 200) {
-        ElMessage.error(`Unable to retrieve RO Crate file`);
-    }
-    data.crate = (await response.json()).crate;
-
-    // load the profile
-    response = await $http.get({
-        route: `/describo/profile/${$route.meta.type}`,
-    });
-    if (response.status !== 200) {
-        ElMessage.error(`Unable to retrieve profile`);
-    }
-    data.profile = (await response.json()).profile;
-}
-async function saveCrate(data) {
-    let response = await $http.put({
-        route: `/describo/rocrate/${$route.meta.type}/${$route.params.identifier}`,
-        body: { data },
-    });
-    if (response.status !== 200) {
-        ElMessage.error(`Unable to save RO Crate file`);
-    }
-    // console.log("SAVE CRATE", JSON.stringify(data, null, 2));
-}
-function saveTemplate(template) {
-    // console.log("SAVE TEMPLATE", JSON.stringify(template, null, 2));
-    data.templates.push(template);
-    console.log(JSON.stringify(data.templates, null, 2));
 }
 </script>
